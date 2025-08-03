aefeefe0243530182444cd08e0d0815bcf2b2681
ramanan-26 <ramanansaravanan26@gmail.com>
2025-08-04 00:45:17 +0530

    Created match model

--- cricbackend/matches/models.py
+++ cricbackend/matches/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Match(models.Model):
+    team_A = models.CharField(max_length=25)
+    team_B = models.CharField(max_length=25)
+    noOfOvers = models.PositiveIntegerField()
+    toss = models.CharField(max_length=25)
+    battingFirst = models.CharField(max_length=25)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.team_A} vs {self.team_B}"
